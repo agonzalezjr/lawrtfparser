c504cb9455599f584e6710a8a457069a382812bb
Andy Gonzalez <andy.gonzalez@sap.com>
2016-10-27 14:49:36 -0400

    Creating the CSV file
    Change-Id: I6a6d44b15be2f3e7613d2b24e617a51abcda120f

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A little utility to parse RTF files
 
 ```
-usage: python rtfparser.py [-h] [-d DIRECTORY]
+usage: rtfparser.py [-h] [-d DIRECTORY] [-o OUTPUT]
 
 Parses RTF files.
 
@@ -10,4 +10,6 @@ optional arguments:
   -h, --help            show this help message and exit
   -d DIRECTORY, --directory DIRECTORY
                         The directory with the RTF files to parse
+  -o OUTPUT, --output OUTPUT
+                        The name of the output CSV file
 ```
--- rtfparser.py
+++ rtfparser.py
@@ -1,18 +1,42 @@
 import argparse
 import os
 import sys
+import csv
 
 parser = argparse.ArgumentParser(description='Parses RTF files.')
 
 # if no 'directory' argument is passed, assume the cwd
 parser.add_argument('-d', '--directory', default=os.getcwd(),
                     help='The directory with the RTF files to parse')
+parser.add_argument('-o', '--output', default='output.csv',
+                    help='The name of the output CSV file')
 
 args = parser.parse_args()
 
+dataFields = ['judge', 'case', 'court'];
+
+# an array of dicts with the result of the run
+data = [];
+
+def dumpData(fileName, data, dataFields):
+    print('Generating ' + fileName + ' ...')
+    with open(fileName, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames = dataFields)
+        writer.writeheader()
+        for row  in data:
+            writer.writerow(row)
+    print('Done.')
+
 def processFile(root, file):
-    # TODO: Everything :)
-    print(os.path.join(root, file))
+    print('Processing file: ' + os.path.join(root, file))
+
+    # TODO: The actual parsing ...
+
+    # For now, assume this is the output for each file
+    dataRow = {}
+    dataRow['judge'] = 'foo'
+    dataRow['case'] = '123'
+    data.append(dataRow)
 
 if not os.path.isdir(args.directory):
     sys.exit('ERROR: passed argument is not a directory (' + args.directory + ')')
@@ -21,3 +45,5 @@ for root, dirs, files in os.walk(args.directory):
     for file in files:
         if file.endswith(".rtf"):
             processFile(root, file)
+
+dumpData(args.output, data, dataFields)
