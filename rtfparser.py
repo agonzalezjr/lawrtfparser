4e8ba7643ea0253e3fd77e5046c6e9ee7e1dfbe8
Andy Gonzalez <andy.gonzalez@sap.com>
2016-10-26 21:08:33 -0400

    traversing directory to find files
    Change-Id: I352ed369ab8b7ffd94309ab6680af8f7b49956a8

--- rtfparser.py
+++ rtfparser.py
@@ -10,5 +10,14 @@ parser.add_argument('-d', '--directory', default=os.getcwd(),
 
 args = parser.parse_args()
 
+def processFile(root, file):
+    # TODO: Everything :)
+    print(os.path.join(root, file))
+
 if not os.path.isdir(args.directory):
     sys.exit('ERROR: passed argument is not a directory (' + args.directory + ')')
+
+for root, dirs, files in os.walk(args.directory):
+    for file in files:
+        if file.endswith(".rtf"):
+            processFile(root, file)
